4730f42f86c10e1d07c1503ff5b0d8be85cc8a7f
pkoby <peterjkoby@gmail.com>
2021-11-11 21:35:35 -0500

    Added FlagFilter

---
+++ FlagFilter/scripts.js
@@ -0,0 +1,85 @@
+// not exactly vanilla as there is one lodash function
+
+var allCheckboxes = document.querySelectorAll('input[type=checkbox]');
+var allRadio = document.querySelectorAll('input[type=radio]');
+var allFlags = Array.from(document.querySelectorAll('.flag'));
+var checked = {};
+
+getChecked('Red');
+getChecked('Orange');
+getChecked('Yellow');
+getChecked('Green');
+getChecked('Blue');
+getChecked('Purple');
+getChecked('Pink');
+getChecked('Brown');
+getChecked('Black');
+getChecked('Gray');
+getChecked('White');
+getChecked('Stripes');
+getChecked('Chevrons');
+getChecked('Symbols');
+getChecked('Count');
+
+Array.prototype.forEach.call(allCheckboxes, function (el) {
+  el.addEventListener('change', toggleCheckbox);
+});
+
+function toggleCheckbox(e) {
+  getChecked(e.target.name);
+  setVisibility();
+}
+
+function getChecked(name) {
+  checked[name] = Array.from(document.querySelectorAll('input[name=' + name + ']:checked')).map(function (el) {
+    return el.value;
+  });
+}
+
+function setVisibility() {
+  allFlags.map(function (el) {
+    var Red = checked.Red.length ? _.intersection(Array.from(el.classList), checked.Red).length : true;
+    var Orange = checked.Orange.length ? _.intersection(Array.from(el.classList), checked.Orange).length : true;
+    var Yellow = checked.Yellow.length ? _.intersection(Array.from(el.classList), checked.Yellow).length : true;
+    var Green = checked.Green.length ? _.intersection(Array.from(el.classList), checked.Green).length : true;
+    var Blue = checked.Blue.length ? _.intersection(Array.from(el.classList), checked.Blue).length : true;
+    var Purple = checked.Purple.length ? _.intersection(Array.from(el.classList), checked.Purple).length : true;
+    var Pink = checked.Pink.length ? _.intersection(Array.from(el.classList), checked.Pink).length : true;
+    var Brown = checked.Brown.length ? _.intersection(Array.from(el.classList), checked.Brown).length : true;
+    var Black = checked.Black.length ? _.intersection(Array.from(el.classList), checked.Black).length : true;
+    var Gray = checked.Gray.length ? _.intersection(Array.from(el.classList), checked.Gray).length : true;
+    var White = checked.White.length ? _.intersection(Array.from(el.classList), checked.White).length : true;
+    var Stripes = checked.Stripes.length ? _.intersection(Array.from(el.classList), checked.Stripes).length : true;
+    var Chevrons = checked.Chevrons.length ? _.intersection(Array.from(el.classList), checked.Chevrons).length : true;
+    var Symbols = checked.Symbols.length ? _.intersection(Array.from(el.classList), checked.Symbols).length : true;
+    var Count = checked.Count.length ? _.intersection(Array.from(el.classList), checked.Count).length : true;
+    if (Red && Orange && Yellow && Green && Blue && Purple && Pink && Brown && Black && Gray && White && Stripes && Chevrons && Symbols && Count) {
+      el.style.display = 'inline-block';
+    } else {
+      el.style.display = 'none';
+    }
+  });
+}
+
+function uncheckAll() {
+  document.querySelectorAll('input[type="checkbox"]')
+    .forEach(el => el.checked = false);
+    getChecked('Red');
+    getChecked('Orange');
+    getChecked('Yellow');
+    getChecked('Green');
+    getChecked('Blue');
+    getChecked('Purple');
+    getChecked('Pink');
+    getChecked('Brown');
+    getChecked('Black');
+    getChecked('Gray');
+    getChecked('White');
+    getChecked('Stripes');
+    getChecked('Chevrons');
+    getChecked('Symbols');
+    getChecked('Count');
+    setVisibility();
+}
+
+document.querySelector('button').addEventListener('click', uncheckAll)
