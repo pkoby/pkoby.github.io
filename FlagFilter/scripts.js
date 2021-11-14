4d12a975bffa08858c1f60a5d057fbac06e2b33f
pkoby <peterjkoby@gmail.com>
2021-11-14 11:14:07 -0500

    Updated data

--- FlagFilter/scripts.js
+++ FlagFilter/scripts.js
@@ -2,12 +2,12 @@
 
 function openNav() {
 	document.getElementById("filterMenu").style.visibility = "visible";
-	// document.getElementById("main").style.marginLeft = "210px";
+	document.getElementById("main").style.marginLeft = "210px";
 }
 
 function closeNav() {
 	document.getElementById("filterMenu").style.visibility = "hidden";
-	// document.getElementById("main").style.marginLeft = "0";
+	document.getElementById("main").style.marginLeft = "0";
 }
 
 var allCheckboxes = document.querySelectorAll('input[type=checkbox]');
@@ -19,6 +19,7 @@ getChecked('Red');
 getChecked('Orange');
 getChecked('Yellow');
 getChecked('Green');
+getChecked('Turquoise');
 getChecked('Blue');
 getChecked('Purple');
 getChecked('Pink');
@@ -36,6 +37,7 @@ Array.prototype.forEach.call(allCheckboxes, function (el) {
 });
 
 function toggleCheckbox(e) {
+	console.countReset(checked);
 	getChecked(e.target.name);
 	setVisibility();
 }
@@ -52,6 +54,7 @@ function setVisibility() {
 		var Orange = checked.Orange.length ? _.intersection(Array.from(el.classList), checked.Orange).length : true;
 		var Yellow = checked.Yellow.length ? _.intersection(Array.from(el.classList), checked.Yellow).length : true;
 		var Green = checked.Green.length ? _.intersection(Array.from(el.classList), checked.Green).length : true;
+		var Turquoise = checked.Turquoise.length ? _.intersection(Array.from(el.classList), checked.Turquoise).length : true;
 		var Blue = checked.Blue.length ? _.intersection(Array.from(el.classList), checked.Blue).length : true;
 		var Purple = checked.Purple.length ? _.intersection(Array.from(el.classList), checked.Purple).length : true;
 		var Pink = checked.Pink.length ? _.intersection(Array.from(el.classList), checked.Pink).length : true;
@@ -63,8 +66,9 @@ function setVisibility() {
 		var Chevrons = checked.Chevrons.length ? _.intersection(Array.from(el.classList), checked.Chevrons).length : true;
 		var Symbols = checked.Symbols.length ? _.intersection(Array.from(el.classList), checked.Symbols).length : true;
 		var Count = checked.Count.length ? _.intersection(Array.from(el.classList), checked.Count).length : true;
-		if (Red && Orange && Yellow && Green && Blue && Purple && Pink && Brown && Black && Gray && White && Stripes && Chevrons && Symbols && Count) {
+		if (Red && Orange && Yellow && Green && Turquoise && Blue && Purple && Pink && Brown && Black && Gray && White && Stripes && Chevrons && Symbols && Count) {
 			el.style.display = 'inline-block';
+			console.count(checked);
 		} else {
 			el.style.display = 'none';
 		}
@@ -72,12 +76,14 @@ function setVisibility() {
 }
 
 function uncheckAll() {
+	console.countReset(checked);
 	document.querySelectorAll('input[type="checkbox"]')
 		.forEach(el => el.checked = false);
 		getChecked('Red');
 		getChecked('Orange');
 		getChecked('Yellow');
 		getChecked('Green');
+		getChecked('Turquoise');
 		getChecked('Blue');
 		getChecked('Purple');
 		getChecked('Pink');
