89af751ad70c6b14f05e77fbb35219a55142406c
pkoby <peterjkoby@gmail.com>
2023-07-30 11:02:59 -0400

    Add files via upload

--- LGBTQmap/mapcode.js
+++ LGBTQmap/mapcode.js
@@ -41,10 +41,10 @@ var Stamen_Toner = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/
 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	subdomains: 'abcd',
 	minZoom: 0,
-	maxZoom: 20,
+	maxZoom: 18,
 	ext: 'png',
-	opacity: 0.2,
-	detectRetina: true,
+	opacity: 0.3,
+	// detectRetina: true,
 });
 
 var Stamen_TonerBackground = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}{r}.{ext}', {
