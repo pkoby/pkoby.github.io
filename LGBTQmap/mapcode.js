adaa0c929e6424dc2b266ae904f505cb8115869b
pkoby <peterjkoby@gmail.com>
2023-07-30 10:37:28 -0400

    Add files via upload

--- LGBTQmap/mapcode.js
+++ LGBTQmap/mapcode.js
@@ -20,6 +20,12 @@ var Positron = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/
 	detectRetina: true,
 	// opacity: 0.3,
 });
+var CartoDB_PositronNoLabels = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+	subdomains: 'abcd',
+	maxZoom: 20,
+	detectRetina: true,
+});
 var CartoDB_DarkMatter = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
 	subdomains: 'abcd',
@@ -94,7 +100,7 @@ var loadingOverlay = L.polygon([
 
 
 var map = new L.map('bigmap', {
-	layers: [CartoDB_DarkMatter],
+	layers: [CartoDB_PositronNoLabels,CartoDB_PositronOnlyLabels],
 	maxBounds: [[90,-180],[-90,180]],
 	zoomControl: false,
 })
